0d8b509671f893ac3c8341fa4d0d24857bc12068
Nilesh <nilbanmadan@gmail.com>
2025-12-18 23:04:51 +0530

    Map Study

---
+++ src/com/practice/basics/MapStudy.java
@@ -0,0 +1,14 @@
+package com.practice.basics;
+
+public class MapStudy {
+
+	public static void main(String[] args) {
+		String s1 = "a";
+		String s2 = "b";
+		String s3 = "a";
+		System.out.println("s1: " + s1.hashCode());
+		System.out.println("s2: " + s2.hashCode());
+		System.out.println("s3: " + s3.hashCode());
+	}
+
+}
